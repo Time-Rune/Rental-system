d77b06253d3b116da85e545990ba19719c32ef79
AGentleCat <zhang.essexclass@gmail.com>
2022-06-28 11:07:43 +0800

    修改了依赖项webapp

--- src/main/java/com/example/rentalsystem/RentalSystemApplication.java
+++ src/main/java/com/example/rentalsystem/RentalSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RentalSystemApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RentalSystemApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSystemApplication.class, args);
+	}
 
 }
