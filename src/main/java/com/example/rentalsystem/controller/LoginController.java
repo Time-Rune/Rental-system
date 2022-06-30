d7a27cecd479d4da2086486e128e494794ca30cf
Hygge <2961540341@qq.com>
2022-06-30 14:33:26 +0800

    轮播图和新闻和房源

--- src/main/java/com/example/rentalsystem/controller/LoginController.java
+++ src/main/java/com/example/rentalsystem/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
         for(int i = 0; i < houses.size(); i++){
             model.addAttribute("Latesthouses" + i, houses.get(i));
         }
-        return "success";
+        return "index";
     }
 //    登录注册
     @PostMapping("/login")
