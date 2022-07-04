d45f5c52639964507338fdbbfe11ba5a555c3c68
Rune <1371497318@qq.com>
2022-07-04 11:25:24 +0800

    后台管理-合同

---
+++ src/main/java/com/example/rentalsystem/controller/ContractReceive.java
@@ -0,0 +1,5 @@
+package com.example.rentalsystem.controller;
+
+
+public class ContractReceive {
+}
--- src/main/java/com/example/rentalsystem/service/ServiceContract.java
+++ src/main/java/com/example/rentalsystem/service/ServiceContract.java
@@ -1,4 +1,37 @@
 package com.example.rentalsystem.service;
 
+import com.example.rentalsystem.entity.Contract;
+import com.example.rentalsystem.repository.ContractSQL;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
 public class ServiceContract {
+    @Resource
+    private ContractSQL contractSQL;
+
+    public void addContract(int uid1, int uid2, int hid, String begin, String end){
+        int newid = contractSQL.getMaxCID() + 1;
+        contractSQL.insertContract(newid, uid1, uid2, begin, end, hid);
+    }
+
+    public void updateContract(int id, int uid1, int uid2, int hid){
+        contractSQL.updateContract(id, uid1, uid2, hid);
+    }
+
+    public void deleteContract(int id){
+        contractSQL.deleteContract(id);
+    }
+
+    public List<Contract> selectAllContract(){
+        return  contractSQL.getContract();
+    }
+
+    public List<Contract> searcthConstract(String key){
+        String key0 = "%" + key + "%";
+        return contractSQL.searchContract(key0);
+    }
+
 }
