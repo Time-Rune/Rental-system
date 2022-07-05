07d5247333f21bd4b79c81bb29f6f3a0f5d85a84
AGentleCat <zhang.essexclass@gmail.com>
2022-07-05 13:47:11 +0800

    主页登录

--- src/main/java/com/example/rentalsystem/repository/loginSQL.java
+++ src/main/java/com/example/rentalsystem/repository/loginSQL.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Mapper
 public interface loginSQL {
-    @Select("select * from User where UID=#{account} and UPassword=#{password}")
+    @Select("select * from User where Uaccount=#{account} and UPassword=#{password}")
     User loginUser(String account, String password);
 
     @Select("select * from admins where Aaccount = #{account} and Apassword = #{password}")
